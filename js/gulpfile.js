1c937280f83915b214b973006219e1ad3328e03a
Joe Blommel <j-blommel@hotmail.com>
2021-11-09 16:14:14 -0500

    started styling and added nav on index

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,11 +1,11 @@
 const {src,dest,watch,series} = require ('gulp')
 const sass = require('gulp-sass')(require('sass'))
 function buildStyles(){
-    return src('sass/*.scss').pipe(sass()).pipe(dest('css'))
+    return src('../sass/*.scss').pipe(sass()).pipe(dest('../css'))
 }
 
 function watchTask(){
-    watch(['sass/*.scss'], buildStyles)
+    watch(['../sass/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles,watchTask)
